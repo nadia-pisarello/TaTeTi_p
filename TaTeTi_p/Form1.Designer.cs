75749953f3fdc3edcb7cef0d6432acafd5cc203e
Nadia Pisarello <pisarello.nads@gmail.com>
2023-07-20 12:04:07 -0300

    Agregar archivos de proyecto.

---
+++ TaTeTi_p/Form1.Designer.cs
@@ -0,0 +1,331 @@
+﻿namespace TaTeTi_p
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tb2 = new System.Windows.Forms.TextBox();
+            this.tb1 = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.btnMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnIniciar = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnReiniciar = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnGuardar = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnSalir = new System.Windows.Forms.ToolStripMenuItem();
+            this.tablero = new System.Windows.Forms.TableLayoutPanel();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.tablero.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Controls.Add(this.menuStrip1);
+            this.panel1.Location = new System.Drawing.Point(1, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(415, 104);
+            this.panel1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.tb2);
+            this.groupBox1.Controls.Add(this.tb1);
+            this.groupBox1.Location = new System.Drawing.Point(2, 36);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(413, 59);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Jugadores";
+            // 
+            // tb2
+            // 
+            this.tb2.Location = new System.Drawing.Point(234, 24);
+            this.tb2.Name = "tb2";
+            this.tb2.Size = new System.Drawing.Size(167, 26);
+            this.tb2.TabIndex = 1;
+            this.tb2.Text = "Jugador O";
+            // 
+            // tb1
+            // 
+            this.tb1.Location = new System.Drawing.Point(30, 24);
+            this.tb1.Name = "tb1";
+            this.tb1.Size = new System.Drawing.Size(167, 26);
+            this.tb1.TabIndex = 0;
+            this.tb1.Text = "Jugador X";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnMenu});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(415, 33);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // btnMenu
+            // 
+            this.btnMenu.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnIniciar,
+            this.btnReiniciar,
+            this.btnGuardar,
+            this.btnSalir});
+            this.btnMenu.Name = "btnMenu";
+            this.btnMenu.Size = new System.Drawing.Size(73, 29);
+            this.btnMenu.Text = "Menú";
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(238, 34);
+            this.btnIniciar.Text = "Iniciar";
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+            // 
+            // btnReiniciar
+            // 
+            this.btnReiniciar.Name = "btnReiniciar";
+            this.btnReiniciar.Size = new System.Drawing.Size(238, 34);
+            this.btnReiniciar.Text = "Reiniciar";
+            this.btnReiniciar.Click += new System.EventHandler(this.btnReiniciar_Click);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(238, 34);
+            this.btnGuardar.Text = "Guardar partida";
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(238, 34);
+            this.btnSalir.Text = "Salir";
+            // 
+            // tablero
+            // 
+            this.tablero.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tablero.ColumnCount = 3;
+            this.tablero.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tablero.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tablero.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tablero.Controls.Add(this.button9, 2, 2);
+            this.tablero.Controls.Add(this.button8, 1, 2);
+            this.tablero.Controls.Add(this.button7, 0, 2);
+            this.tablero.Controls.Add(this.button6, 2, 1);
+            this.tablero.Controls.Add(this.button5, 1, 1);
+            this.tablero.Controls.Add(this.button4, 0, 1);
+            this.tablero.Controls.Add(this.button3, 2, 0);
+            this.tablero.Controls.Add(this.button2, 1, 0);
+            this.tablero.Controls.Add(this.button1, 0, 0);
+            this.tablero.Location = new System.Drawing.Point(1, 101);
+            this.tablero.Name = "tablero";
+            this.tablero.RowCount = 3;
+            this.tablero.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tablero.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tablero.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tablero.Size = new System.Drawing.Size(415, 365);
+            this.tablero.TabIndex = 1;
+            // 
+            // button9
+            // 
+            this.button9.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button9.Font = new System.Drawing.Font("Snap ITC", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.Location = new System.Drawing.Point(279, 245);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(133, 117);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "¿?";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.Jugada_Click);
+            // 
+            // button8
+            // 
+            this.button8.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button8.Font = new System.Drawing.Font("Snap ITC", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.Location = new System.Drawing.Point(141, 245);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(132, 117);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "¿?";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.Jugada_Click);
+            // 
+            // button7
+            // 
+            this.button7.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button7.Font = new System.Drawing.Font("Snap ITC", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(3, 245);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(132, 117);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "¿?";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.Jugada_Click);
+            // 
+            // button6
+            // 
+            this.button6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button6.Font = new System.Drawing.Font("Snap ITC", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(279, 124);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(133, 115);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "¿?";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.Jugada_Click);
+            // 
+            // button5
+            // 
+            this.button5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button5.Font = new System.Drawing.Font("Snap ITC", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(141, 124);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(132, 115);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "¿?";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Jugada_Click);
+            // 
+            // button4
+            // 
+            this.button4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button4.Font = new System.Drawing.Font("Snap ITC", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(3, 124);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(132, 115);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "¿?";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Jugada_Click);
+            // 
+            // button3
+            // 
+            this.button3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button3.Font = new System.Drawing.Font("Snap ITC", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(279, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(133, 115);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "¿?";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Jugada_Click);
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button2.Font = new System.Drawing.Font("Snap ITC", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(141, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(132, 115);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "¿?";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Jugada_Click);
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Font = new System.Drawing.Font("Snap ITC", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 115);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "¿?";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Jugada_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(416, 466);
+            this.Controls.Add(this.tablero);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MinimumSize = new System.Drawing.Size(397, 479);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TaTeTí";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tablero.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem btnMenu;
+        private System.Windows.Forms.ToolStripMenuItem btnIniciar;
+        private System.Windows.Forms.ToolStripMenuItem btnReiniciar;
+        private System.Windows.Forms.ToolStripMenuItem btnGuardar;
+        private System.Windows.Forms.ToolStripMenuItem btnSalir;
+        private System.Windows.Forms.TableLayoutPanel tablero;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox tb2;
+        private System.Windows.Forms.TextBox tb1;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ TaTeTi_p/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TaTeTi_p
+{
+    public partial class Form1 : Form
+    {
+        string turno;
+        string ganador;
+        bool iniciar = false;
+        List<Button> buttonList = new List<Button>();
+        int moves;
+        DateTime inicioPartida;
+        DateTime finalPartida;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            iniciar = true;
+            turno = "O";
+            ListarBotones();
+            LimpiarBotones();
+            tb1.ReadOnly = true;
+            tb2.ReadOnly = true;
+            moves = 9;
+            inicioPartida = DateTime.Now;
+        }
+
+        private void btnReiniciar_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("¿Seguro quieren reinicar?", "Reiniciar", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+            if (result == DialogResult.Cancel) return;
+            Reiniciar();
+        }
+
+        private void Jugada_Click(object sender, EventArgs e)
+        {
+            if (!iniciar)
+            {
+                MessageBox.Show("Ingresa al menú y selecciona Iniciar");
+                return;
+            }
+            if (moves > 0 && !Ganador())
+            {
+                Button btn = (Button)sender;
+                turno = turno == "O" ? "X" : "O";
+                btn.Text = turno;
+                moves--;
+                finalPartida = DateTime.Now;
+            }
+            if (Ganador())
+            {
+                ganador = turno == "O" ? tb2.Text : tb1.Text;
+                finalPartida = DateTime.Now;
+                MessageBox.Show(string.Format("¡{0} ha ganado!", ganador), "Ganador");
+
+            }
+            if (moves == 0 && !Ganador())
+            {
+                finalPartida = DateTime.Now;
+                MessageBox.Show("Han empatado", "Empate");
+            }
+        }
+
+        private void ListarBotones()
+        {
+            foreach (Control control in tablero.Controls)
+            {
+                Button button = control as Button;
+                buttonList.Add(button);
+            }
+        }
+
+        private bool Ganador()
+        {
+            return (buttonList[0].Text == turno && buttonList[0].Text == buttonList[1].Text && buttonList[1].Text == buttonList[2].Text) ||
+          (buttonList[3].Text == turno && buttonList[3].Text == buttonList[4].Text && buttonList[4].Text == buttonList[5].Text) ||
+          (buttonList[6].Text == turno && buttonList[6].Text == buttonList[7].Text && buttonList[7].Text == buttonList[8].Text) ||
+          (buttonList[0].Text == turno && buttonList[0].Text == buttonList[3].Text && buttonList[3].Text == buttonList[6].Text) ||
+          (buttonList[1].Text == turno && buttonList[1].Text == buttonList[4].Text && buttonList[4].Text == buttonList[7].Text) ||
+          (buttonList[2].Text == turno && buttonList[2].Text == buttonList[5].Text && buttonList[5].Text == buttonList[8].Text) ||
+          (buttonList[0].Text == turno && buttonList[0].Text == buttonList[4].Text && buttonList[4].Text == buttonList[8].Text) ||
+          (buttonList[2].Text == turno && buttonList[2].Text == buttonList[4].Text && buttonList[4].Text == buttonList[6].Text);
+        }
+
+        private void Reiniciar()
+        {
+            turno = "O";
+            moves = 9;
+            LimpiarBotones();
+        }
+
+        private void LimpiarBotones()
+        {
+            foreach (Button button in buttonList)
+            {
+                button.Text = "";
+            }
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            Guardar();
+        }
+
+        private void Guardar()
+        {
+            string path = string.Empty;
+            SaveFileDialog saveFile = new SaveFileDialog();
+            saveFile.Filter = "Archivo de texto (*.txt) | *.txt";
+            saveFile.ShowDialog();
+            path = saveFile.FileName;
+            if (!String.IsNullOrEmpty(path))
+            {
+                using (StreamWriter sw = new StreamWriter(path))
+                {
+                    int contador = 0;
+                    List<Button> listaDeBotones = new List<Button>();
+                    listaDeBotones = buttonList;
+                    listaDeBotones.Reverse();
+                    sw.WriteLine("Judagores: {0}, {1}", tb1.Text, tb2.Text);
+                    sw.WriteLine("Ganador {0}", ganador);
+                    sw.WriteLine("Jugada: \r\n");
+                    sw.WriteLine("\r\n-- -- -- -- -- -- -- -- --");
+
+                    foreach (Button button in listaDeBotones)
+                    {
+
+                        sw.Write("   " + button.Text + "  |  ");
+                        contador++;
+                        if (contador == 3)
+                        {
+                            contador = 0;
+                            sw.WriteLine("\r\n-- -- -- -- -- -- -- -- --");
+                        }
+
+                    }
+                    sw.WriteLine("\r\nInicio Partida: " + inicioPartida.ToString());
+                    sw.WriteLine("Final Partida: " + finalPartida.ToString());
+                    sw.WriteLine("Duracion Partida: {0:F2} segundos", (finalPartida - inicioPartida).TotalSeconds);
+                }
+
+            }
+        }
+    }
+}
